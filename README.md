3493c20291d1ff7248d5f8abb85fbe4101371a4c
Christoffer Åhrling <christoffer@nilpath.se>
2013-01-28 18:28:58 +0100

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-jQuery-Randomizer
-=================
+# Randomizer Plugin
 
-A jQuery plugin made for pick something from a list at random
+A jQuery plugin made for pick something from a list at random.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/nilpath/jQuery-Randomizer/master/dist/randomizer-plugin.0.1.0.min.js
+[max]: https://raw.github.com/nilpath/jQuery-Randomizer/master/dist/randomizer-plugin.0.1.0.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/randomizer-plugin.0.1.0.min.js"></script>
+<script>
+jQuery(function($) {
+  $(div).randomizer(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ dist/randomizer-plugin.0.1.0.js
@@ -0,0 +1,61 @@
+/*! jQuery Randomizer Plugin - v0.1.0 - 2013-01-28
+* https://github.com/nilpath/jQuery-Randomizer
+* Copyright (c) 2013 Christoffer Åhrling; Licensed MIT */
+
+(function($) {
+
+	$.fn.randomizer = function(options){
+		var audio = $('<audio>');
+		var source = $('<source>');
+		var heading = $('<h1>');
+		var btn = $('<a>');
+		var interval = null;
+		
+		// Settings
+		var settings = $.extend({
+			'audio' : {
+				'play' : true,
+				'url' : 'http://www.eugeneandtex.com/drumroll-15.mp3',
+				'type' : 'audio/mpeg'
+			},
+			'interval' : 10000,
+			'values' : null
+		}, options);
+		
+		// Methods
+		var clear = function(){
+			clearInterval(interval);	
+		};
+
+		var appendHtml = function(){
+			var rand = Math.floor(Math.random()*settings.values.length);
+			heading.html(settings.values[rand]);	
+		};
+		
+		// Elements Setup
+		source[0].src = settings.audio.url;
+		source[0].type = settings.audio.type;
+		audio.css('display', 'none').append(source).addClass('randomizer-sound');
+		
+		heading.attr('id', 'randomizer-selected');
+
+		btn.attr('id', 'randomizer-btn').html('Random').on('click',function(e){
+			e.preventDefault();
+			if(settings.audio.play){
+				audio[0].load();
+				audio[0].play();
+			}
+			setTimeout(function(){clear();}, settings.interval);
+			interval = setInterval(function(){appendHtml();}, 100);
+		});
+
+		this.append(heading);
+		this.append(btn);
+		if(settings.audio.play) {
+			this.append(audio);
+		}
+
+
+	};
+
+}(jQuery));
